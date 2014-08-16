5d6041c6286f65870d135b9287f92a9bf986b2ea
btibi <ihrwein@gmail.com>
2014-08-16 14:21:28 +0200

    syslog-ng: Refactor configuration generation logic.
    The old method was hard to maintain, so this commit introduces a more clean solution.
    The commit also contains documentation fixes and new testcases.

--- states/syslog_ng.py
+++ states/syslog_ng.py
@@ -5,11 +5,11 @@ State module for syslog_ng
 
 :maintainer:    Tibor Benke <btibi@sch.bme.hu>
 :maturity:      new
-:depends:       cmd, ps
+:depends:       cmd, ps, syslog_ng
 :platform:      all
 
-Users can generate syslog-ng configuration files from YAML format by using
-this module or use plain ones and reload, start, or stop their syslog-ng.
+Users can generate syslog-ng configuration files from YAML format or use
+ plain ones and reload, start, or stop their syslog-ng by using this module.
 
 Details
 -------
@@ -21,13 +21,13 @@ and :mod:`syslog_ng.started <salt.states.syslog_ng.started>` functions.
 If the service module is available on the computers, users should use that.
 
 Syslog-ng can be installed via a package manager or from source. In the
-latter case, the syslog-ng and syslog-ng-ctl binaries are not available
+latter case, syslog-ng and syslog-ng-ctl binaries are not available
 from the PATH, so users should set location of the sbin directory with
 :mod:`syslog_ng.set_binary_path <salt.states.syslog_ng.set_binary_path>`.
 
 Similarly, users can specify the location of the configuration file with
 :mod:`syslog_ng.set_config_file <salt.states.syslog_ng.set_config_file>`, then
-the module will use it. If it is not set, syslog-ng use the default
+the module will use it. If it is not set, syslog-ng uses the default
 configuration file.
 
 For more information see :doc:`syslog-ng state usage </topics/tutorials/syslog_ng-state-usage>`.
@@ -49,7 +49,8 @@ These constructions are also called statements. There are options inside of them
         option(parameter1, parameter2); option2(parameter1, parameter2);
 
 You can find more information about syslog-ng's configuration syntax in the
-Syslog-ng Admin guide: http://www.balabit.com/sites/default/files/documents/syslog-ng-ose-3.5-guides/en/syslog-ng-ose-v3.5-guide-admin/html-single/index.html#syslog-ng.conf.5
+Syslog-ng Admin guide:
+http://www.balabit.com/sites/default/files/documents/syslog-ng-ose-3.5-guides/en/syslog-ng-ose-v3.5-guide-admin/html-single/index.html#syslog-ng.conf.5
 '''
 
 from __future__ import generators, print_function, with_statement
@@ -71,20 +72,6 @@ def config(name,
     otherwise just returns it
     '''
     return __salt__['syslog_ng.config'](name, config, write)
-
-
-def write_config(name, config, newlines=2):
-    '''
-    Writes the given parameter config into the config file.
-    '''
-    return __salt__['syslog_ng.write_config'](name, config, newlines)
-
-
-def write_version(name):
-    '''
-    Removes the previous configuration file, then creates a new one and writes the name line.
-    '''
-    return __salt__['syslog_ng.write_version'](name)
 
 
 def stopped(name=None):
